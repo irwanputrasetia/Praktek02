6ad41520c5e55094dd8fa40d1de520f9247ddb24
irwanputrasetia <irwanputrastya@gmail.com>
2018-03-30 17:39:05 +0700

    Membuat Class Aksi dan Method Main

---
+++ src/praktek02/TanahAksi.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek02;
+
+public class TanahAksi {
+    public static void main(String[] args) {
+        
+    }
+}
