de06d348311c5e18973023a220cbae11e0440f1f
irwanputrasetia <irwanputrastya@gmail.com>
2018-03-30 17:36:31 +0700

    Membuat Attribut dan Behavior

---
+++ src/praktek02/Tanah.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek02;
+
+public class Tanah {
+   double PanjangTanah;
+   double LebarTanah;
+   
+  void cetakInfo(){
+       System.out.println("=============================");
+       System.out.println("Panjang Tanah :"+PanjangTanah);
+       System.out.println("Lebar Tanah :"+LebarTanah);
+       System.out.println("=============================");
+   }
+}
